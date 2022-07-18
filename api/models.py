b6d08dc221d2946de708a7b6a0fd5f2476caa100
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-18 15:03:01 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
+
 from django.db import models
 
 # Create your models here.
+
+class Api(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    Address = models.CharField(max_length=50)
+
