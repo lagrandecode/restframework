53119d57eaa7d446414c2fe60bfd8798d8588ed1
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-18 15:02:54 +0100

    "commit"

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,8 @@
-from django.contrib.auth.models import User, Group
+
 from rest_framework import serializers
+from .models import Api
+
+class Apiserializer(serializers.HyperlinkModelSerializer):
+    class Meta:
+        model = Api
+        fields = ['name','title','Address']
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Api
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.serializers import Apiserializer
